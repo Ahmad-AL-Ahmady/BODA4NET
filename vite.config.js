f5e9ca027e07dc57682f59bcafbff51aa44f2046
Ahmad-AL-Ahmady <ahmad.hussain.alahmady@gmail.com>
2025-08-06 17:31:20 +0300

    Update package.json to include tw-animate-css dependency and clean up vite.config.js by removing unnecessary entries from manualChunks.

--- vite.config.js
+++ vite.config.js
@@ -23,11 +23,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ["react", "react-dom"],
-          ui: [
-            "@radix-ui/react-dialog",
-            "@radix-ui/react-select",
-            "@radix-ui/react-button",
-          ],
+          ui: ["@radix-ui/react-dialog", "@radix-ui/react-select"],
           utils: ["sweetalert2", "framer-motion"],
         },
       },
